40f68f22efb92b436bc0eb855600d3f9f210094b
Glonel Dimapilis <gdimapilis@aol.com>
2023-02-24 16:57:11 -0600

    Still figuring out the server-side coding for operations and display

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,52 @@
 console.log('Wassup, world?!');
+
+
+
+// Functions for operations
+function addNumbers(event) {
+    let firstNumber = document.querySelector('#first-number').value;
+    let secondNumber = document.querySelector('#second-number').value;
+    console.log(firstNumber, secondNumber);
+
+    let numbers = {
+        first: firstNumber,
+        second: secondNumber
+    };
+
+    axios.post('/numbers', numbers).then((response) => {
+        console.log(response);
+        getResult();
+    })
+}
+
+function subNumber(event) {
+    let firstNumber = document.querySelector('#first-number').value;
+    let secondNumber = document.querySelector('#second-number').value;
+    console.log(firstNumber, secondNumber);
+}
+
+function multNumber(event) {
+    let firstNumber = document.querySelector('#first-number').value;
+    let secondNumber = document.querySelector('#second-number').value;
+    console.log(firstNumber, secondNumber);
+}
+
+function divNumber(event) {
+    let firstNumber = document.querySelector('#first-number').value;
+    let secondNumber = document.querySelector('#second-number').value;
+    console.log(firstNumber, secondNumber);
+    let result = firstNumber / secondNumber;
+    return result;
+}
+
+//Function for getResult()
+function getResult() {
+    axios.get( '/numbers' ).then((response) => {
+        console.log('running in getResult()');
+    let resultServer = response.data;
+    let outputDiv = document.querySelector('#resultDiv');
+    outputDiv.innerHTML += `
+    <h2>${resultServer}</h2>
+    `;
+    })
+}
--- server/server.js
+++ server/server.js
@@ -4,16 +4,30 @@ const port = process.env.PORT || 5001;
 //!This app.use will allow req.body to be used. IMPORTANT
 app.use(express.json());
 
-//? Between this first bit of code and the following is
-//? where we post the request functions. 
-//? Everything here will be:
-//? GET requests
-//? POST requests
-//? PUT requests (presumably)
-//? DELETE requests (presumably)
-
 app.use(express.static('server/public'));
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`);
 });
+
+//Global Variables
+
+let resultHistory = [];
+
+//! It seems like the app.get is only here to send back the array info.
+//! This seems to be done AFTER the initial push of the numbers 
+//! object into the resultHistory array.
+app.get('/numbers', (req, res) => {
+    res.send(resultHistory)
+})
+
+
+//TODO I need to figure out how to get the operation going 
+//TODO AND figure out how to display the operation.
+app.post( '/number', (req, res) => {
+    console.log('POST request made ofr /numbers');
+    console.log(req.body);
+    let numbersObject = req.body;
+    resultHistory.push(numbersObject);
+    res.sendStatus(201);
+})
